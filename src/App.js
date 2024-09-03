731a4c37bac156975e757e507af3cbb42d510d70
RockGyuLee <rmfoszld@naver.com>
2024-09-04 00:26:26 +0900

    Apply .gitignore

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <h1>GitHub Pages로 리액트 프로젝트 배포</h1>
+      {/* <header className="App-header">
+        <div className='text-3xl font-bold'>test</div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +19,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
